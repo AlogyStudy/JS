8d2f40c8c140e80f2827c4028c753734210a541d
Xingzhang Lin <1129507496@qq.com>
2016-06-07 22:27:38 +0800

    change app.js

--- nodejs/cookie/cookie.js
+++ nodejs/cookie/cookie.js
@@ -7,10 +7,8 @@ var app = express();
 
 app.use(cookieParser());
 
-
 app.get('/',function( req,res ){
 
-	console.log( req.cookies.username );
 	if( !req.cookies.username ){
 
 		res.send('您还未登陆');
@@ -32,7 +30,5 @@ app.get('/login/:usename',function( req,res ){
 	res.send('登陆成功');
 
 });
-
-
 
 app.listen(1221);
