06fa14c7bbf3f92cf7b13924457cdd9597a600d3
Xingzhang Lin <1129507496@qq.com>
2017-04-13 17:14:51 +0800

    add prototype extend

--- module/ryo.js
+++ module/ryo.js
@@ -1,5 +1,9 @@
 (function(w) {
 	
+	var version = '0.0.1';
+	var arr = [];
+	var slice = arr.slice;
+	
 	/**
 	 * 工厂函数
 	 */
@@ -9,9 +13,103 @@
 	
 	// 挂载原型   // 替换原型 + 原型简称
 	jQuery.fn = jQuery.prototype = {
-		constructor: jQuery
+		constructor: jQuery,
+		
+		jquery: version,
+		
+		selector: '',
+		
+		length: 0,
+		
+		/**
+		 * 转数组
+		 * @return {Array}
+		 */
+		toArray: function () {
+			return slice.call(this);
+		},
+		
+		/**
+		 * 遍历实例，把遍历到的数据分别传给回调使用
+		 * @param {Object} fn
+		 * @return {}
+		 */
+		each: function (fn) {
+			return jQuery.each(this, fn);
+		},
+		
+		map: function (){
+				
+		},
+		
+		/**
+		 * 获取原生对象
+		 * @param {Number} i 
+		 */
+		get: function (i) {
+			// 1. 传入null或undefined，转换为数组返回
+			if (i == null) {
+				return this.toArray();
+			}
+			if (i >= 0) {
+				// 2. 正数，按照指定下标获取元素返回
+				return this[i];
+			} else {
+				// 3. 负数，按照下标倒着（this.length+负数）获取元素返回
+				return this[this.length + i];
+			}
+		},
+		
+		/**
+		 * 截取实例的部分元素，构成一个新的JQ对象。
+		 */
+		slice: function () { // 包头不包尾
+			// 1. 通过数组slice获取部分元素
+			// 2. 把截取到的元素转换为实例对象返回.
+			var nodes = [].slice.apply(this, arguments);  // 参数不固定， 借用数组的slice.
+			return jQuery(nodes);
+		},
+		
+		/**
+		 * 获取指定下标的元素，获取的是JQ类型的实例对象
+		 * @param {Object} num
+		 * @return {Object} 
+		 */
+		eq: function (num) {
+			// 1. 传入null或undefined，那么转换为新实例返回
+			if (num == null) {
+				return jQuery();
+			}
+			
+			if (num >= 0) {
+				// 2. 正数，按照指定下标获取元素，包装成实例返回.
+				return jQuery(this[num]);	
+			} else {
+				// 3. 负数，按照下标倒着（this.length+负数），获取元素，包装成实例返回.
+				return jQuery(this[this.length + num]);
+			}
+		},
+		
+		/**
+		 * 获取实例中的第一个元素，是JQ类型的实例对象
+		 * @return {Object}
+		 */
+		first: function () {
+			return this.eq(0);			
+		},
+		
+		/**
+		 * 获取实例中的最后一个元素，是JQ类型的实例对象
+		 *  @return {Object}
+		 */
+		last: function () {
+			return this.eq(-1);
+		},
+		
+		push: [].push, // 不需要使用call或者apply。因为实例调用中this就是该对象，无须改变。
+		sort: [].sort,
+		splice: [].splice,
 	}
-	
 	
 	/**
 	 * 继承
@@ -24,7 +122,6 @@
 			this[key] = obj[key];
 		}
 	}
-	
 	
 	// JQ对象添加静态方法
 	jQuery.extend({
@@ -40,7 +137,7 @@
 				return true;
 			}
 			return false;
-		}
+		},
 		/**
 		 * 判断是否是函数
 		 * @param {Object} fn
@@ -48,7 +145,7 @@
 		 */
 		isFunction: function(fn) {
 			return !!fn && typeof fn === 'function' ? true : false;
-		}
+		},
 		
 		/**
 		 * 判断是否是window对象
@@ -57,7 +154,7 @@
 		 */
 		isWindow: function(win) {
 			return !!win && win.window == win ? true : false;
-		}
+		},
 		
 		/**
 		 * 判断是否是对象
@@ -69,20 +166,7 @@
 				return true;
 			}
 			return false;
-		}
-		
-		/**
-		 * 去除前后端空格  
-		 * @param {Object} str
-		 */
-		trim: function (str) {
-			var reg = /&\s+|\s+$/g;
-			if (!str) return str;
-			if (str.trim) {
-				return str.trim();	
-			}
-			return str.replace(reg, '');
-		}
+		},
 		
 		/**
 		 * 判断是否是 数组 或类数组
@@ -91,7 +175,7 @@
 		 */
 		isLikeArray: function(arr) {
 			// 函数，window，非数组
-			if (init.isFunction(arr) || init.isWindow(arr) || !init.isObject(arr)) {
+			if (jQuery.isFunction(arr) || jQuery.isWindow(arr) || !jQuery.isObject(arr)) {
 				return false;
 			}
 			// 数组
@@ -103,8 +187,68 @@
 				return true;
 			}
 			return false;
-		}
+		},
 			
+		/**
+		 * 去除前后端空格  
+		 * @param {Object} str
+		 */
+		trim: function (str) {
+			var reg = /&\s+|\s+$/g;
+			if (!str) return str;
+			if (str.trim) {
+				return str.trim();	
+			}
+			return str.replace(reg, '');
+		},
+		
+		/**
+		 * ready函数 
+		 * @param {Function} fn
+		 */
+		ready: function (fn) {
+			if (document.readyState === 'complete') { // DOM是否执行完毕， 通过document.readyState 
+				fn && fn();
+			} else {
+				if (document.addEventListener) {
+					document.addEventListener('DOMContentLoaded', function () {
+						fn && fn();
+					});
+				} else {
+					document.attachEvent('onreadystatechang', function () {
+						if (document.readyState === 'complete') { //  防止onreadystatechang事件多次触发fn多次执行
+							fn && fn();
+						}
+					});
+				}
+			}
+		},
+		
+		/**
+		 * each遍历方法
+		 * @param {Object} obj
+		 * @param {Object} callback
+		 */
+		each: function(obj, callback) {
+			var length, i; // 提前声明 避免预解析相关
+			
+			if (jQuery.isLikeArray(obj)) {
+				length = obj.length;
+				for (i=0; i<length; i++) {
+					if (callback.call(obj[i], i, obj[i]) == false) {
+						break;
+					}
+				}
+			} else {
+				for (i in obj) {
+					if (callback.call(obj[i], i, obj[i]) == false) {
+						break;
+					}
+				}
+			}
+			return obj;
+		},
+		
 	});
 	
 	/**
@@ -116,10 +260,15 @@
 			return this;
 		}
 		
+		// function
+		if (jQuery.isFunction(selector)) {
+			jQuery.ready(selector);	
+		}
+		
 		// string
 		if (typeof selector === 'string') {
-			selector = init.trim(selector);
-			if (init.isHTML(selector)) {
+			selector = jQuery.trim(selector);
+			if (jQuery.isHTML(selector)) {
 			// html fragment
 				var oDiv = document.createElement('div'); // 临时div创建dom，把创建好的dom依次push给实例.
 				oDiv.innerHTML = selector;
@@ -136,23 +285,21 @@
 					return this;
 				}
 			}
-		} else if (init.isLikeArray(arr)) {
+		} else if (jQuery.isLikeArray(selector)) {
 		// array 类数组
-			[].push.apply(this, arr);
+			[].push.apply(this, selector);
 			return this;
 		} else {
 		// other
 			// this[0] = selector;	
 			// this.length = 1;
-			[].push.call(this, selector);
+			[].push.call(this, [].slice.call(selector));
 			return this;
 		}
 	}
 	
 	// 扩展对象  // 替换构造函数的原型为工厂的原型，作用：外界可以通过工厂给实例添加扩展方法.
 	init.prototype = jQuery.fn;
-	
-
 	
 	// 暴露函数
 	w.jQuery = w.$ = jQuery;
